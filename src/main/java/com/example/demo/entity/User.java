efb799698d1a931335370c700e26b27d37c724e2
bala-amypo <bala@amypo.in>
2025-12-23 04:29:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,39 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Vehicle> vehicles;
+
+    // No-arg constructor
+    public User() {}
+
+    // Parameterized constructor (JUST assigns)
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // getters & setters
+}
