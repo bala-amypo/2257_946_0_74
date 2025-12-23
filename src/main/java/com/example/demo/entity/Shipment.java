f24f551aec2ecc014f9a5728af505df3a5e9cd80
bala-amypo <bala@amypo.in>
2025-12-23 08:45:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Shipment.java
+++ src/main/java/com/example/demo/entity/Shipment.java
@@ -1,9 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import lombok.*;
 
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "shipments")
 public class Shipment {
@@ -12,99 +16,19 @@ public class Shipment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Vehicle assigned to shipment
     @ManyToOne(optional = false)
     @JoinColumn(name = "vehicle_id")
     private Vehicle vehicle;
 
-    // Pickup location
     @ManyToOne(optional = false)
     @JoinColumn(name = "pickup_location_id")
     private Location pickupLocation;
 
-    // Drop location
     @ManyToOne(optional = false)
     @JoinColumn(name = "drop_location_id")
     private Location dropLocation;
 
-    // Shipment weight
     private double weightKg;
 
-    // Scheduled shipment date
     private LocalDate scheduledDate;
-
-    // Auto timestamp
-    private LocalDateTime createdAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    // --------- Constructors ---------
-    public Shipment() {}
-
-    public Shipment(Long id, Vehicle vehicle, Location pickupLocation,
-                    Location dropLocation, double weightKg, LocalDate scheduledDate) {
-        this.id = id;
-        this.vehicle = vehicle;
-        this.pickupLocation = pickupLocation;
-        this.dropLocation = dropLocation;
-        this.weightKg = weightKg;
-        this.scheduledDate = scheduledDate;
-    }
-
-    // --------- Getters & Setters ---------
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
-    }
-
-    public Location getPickupLocation() {
-        return pickupLocation;
-    }
-
-    public void setPickupLocation(Location pickupLocation) {
-        this.pickupLocation = pickupLocation;
-    }
-
-    public Location getDropLocation() {
-        return dropLocation;
-    }
-
-    public void setDropLocation(Location dropLocation) {
-        this.dropLocation = dropLocation;
-    }
-
-    public double getWeightKg() {
-        return weightKg;
-    }
-
-    public voi setWeightKg(double weightKg) {
-        this.weightKg = weightKg;
-    }
-
-    public LocalDate getScheduledDate() {
-        return scheduledDate;
-    }
-
-    public void setScheduledDate(LocalDate scheduledDate) {
-        this.scheduledDate = scheduledDate;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
 }
