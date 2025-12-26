41a0389fa2a8d46f5badbdaf08a9922e1ab6b933
bala-amypo <bala@amypo.in>
2025-12-26 09:44:52 +0000

    User commit

--- src/test/TestResultListener.java
+++ src/test/TestResultListener.java
@@ -5,4 +5,3 @@ public class TestResultListener implements ITestListener {
     public void onTestSuccess(ITestResult result) { System.out.println("PASSED: " + result.getName()); }
     public void onTestFailure(ITestResult result) { System.out.println("FAILED: " + result.getName()); }
 }
-EOF
